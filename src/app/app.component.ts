980fe6a0eaedf938d5de06c304041c40ae787f0f
Carlos Garcia <carlosgarlem@gmail.com>
2019-09-16 20:10:13 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {storage} from '../data/localStorage';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'crud-places';
+  public data:any = [];
+  public text: string = "";
+
+  ngOnInit():void{
+    this.data = storage
+  }
+
+   addElement() {
+    this.data.push({
+        mensaje: this.text
+      })
+  }
+
 }
---
+++ src/data/localStorage.ts
@@ -0,0 +1,17 @@
+export const storage: any = [
+    {
+        country: 'Guatemala',
+        place: 'Tikal',
+        description: 'Sitio arqueológico'
+    },
+    {
+        country: 'Corea del Sur',
+        place: 'Oryukdo'
+        description: 'Sky walk'
+    },
+    {
+        country: 'Perú',
+        place: 'Región Cusco'
+        description: 'Vinicunca, montaña arcoiris'
+    }
+]
